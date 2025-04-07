fb4ac2cf5108dc7a46a995bfb0c26c5abbf86f40
evelynvagner013 <evelynvagner2006@gmail.com>
2025-04-07 17:14:45 -0300

    Atualização nas telas

--- src/login/login/login.component.ts
+++ src/login/login/login.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgxMaskDirective, NgxMaskPipe, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isFlipped = false;
 
 }
